5ae12246d2de761b266b775373aefeba001caf10
HsiangChih Cheng <musicmilif@gmail.com>
2019-10-15 16:36:00 +0800

    [REFACTOR] lightgbm regressor and classifier

--- src/moew/models/__init__.py
+++ src/moew/models/__init__.py
@@ -1,25 +1,25 @@
-# # Regressor
-# from sklearn_model import (
-#     SVR,
-#     DecisionTreeRegressor,
-#     ExtraTreeRegressor,
-#     RandomForestRegressor,
-#     ExtraTreesRegressor,
-#     AdaBoostRegressor,
-#     GradientBoostingRegressor,
-#     LinearRegression,
-# )
+# Regressor
+from .sklearn_model import (
+    SVR,
+    DecisionTreeRegressor,
+    ExtraTreeRegressor,
+    RandomForestRegressor,
+    ExtraTreesRegressor,
+    AdaBoostRegressor,
+    GradientBoostingRegressor,
+    LinearRegression,
+)
 from .lgb_model import LGBMRegressor
 
-# # Classifier
-# from sklearn_model import (
-#     SVC,
-#     DecisionTreeClassifier,
-#     ExtraTreeClassifier,
-#     RandomForestClassifier,
-#     ExtraTreesClassifier,
-#     AdaBoostClassifier,
-#     GradientBoostingClassifier,
-#     LogisticRegression,
-# )
+# Classifier
+from .sklearn_model import (
+    SVC,
+    DecisionTreeClassifier,
+    ExtraTreeClassifier,
+    RandomForestClassifier,
+    ExtraTreesClassifier,
+    AdaBoostClassifier,
+    GradientBoostingClassifier,
+    LogisticRegression,
+)
 from .lgb_model import LGBMClassifier
